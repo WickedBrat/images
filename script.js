f5f05fb8f1dd219dd78341d9ceb4aacf9f3d56a7
WickedBrat <harshsrivastav123@gmail.com>
2026-04-05 23:30:54 +0530

    update models

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function generateImageWithGemini(prompt) {
     const response = await ai.models.generateImages({
-        model: 'gemini-3-pro-image-preview',
+        model: 'gemini-3-pro-preview',
         prompt: prompt,
         config: {
             numberOfImages: 1,
